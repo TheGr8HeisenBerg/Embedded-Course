359c1071983a0c1abba0c777ada6ed11639d7f39
TheGr8HeisenBerg <42723307+TheGr8HeisenBerg@users.noreply.github.com>
2021-01-07 02:12:49 +0200

    updated description in solution for question 10 and added structure file for question 11

--- Assignment 2/Question_10/src/Question_10.c
+++ Assignment 2/Question_10/src/Question_10.c
@@ -10,6 +10,8 @@ paths in the digit. Their values are:
 - 1, 2, 3, 5 and 7 = 0 holes.
 - 0, 4, 6, and 9 = 1 hole.
 - 8 = 2 holes.
+Given a number, you must determine the sum of the number of holes for
+all of its digits. For example, the number 819 has 3 holes.
  ============================================================================
  */
 
---
+++ Assignment 2/Question_11/src/Question_11.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Question_11.c
+ Author      : Youssef El-Nemaky
+ Description :
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+	return 0;
+}
