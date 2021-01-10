abc0f1a322838ea692c49f580e976e4c83203495
Youssef  El-Nemaky <yousef.mmdoh@yahoo.com>
2021-01-11 01:51:04 +0200

    added solution for question 6 and updated question 5

--- Assignment 3/Question_06/src/Question_06.c
+++ Assignment 3/Question_06/src/Question_06.c
@@ -2,12 +2,12 @@
  ============================================================================
  Name        : Question_05.c
  Author      : Youssef El-Nemaky
- Description : Write a C function to return the index of FIRST occurrence of a
+ Description : Write a C function to return the index of LAST occurrence of a
  number in a given array. Array index start from 0. If the item is not
  in the list return -1. (Linear Search Algorithm)
  Example:
  Array = {1,2,3,4,4,4}
- The required number is 4 it should return 3
+ The required number is 4 it should return 5
  ============================================================================
  */
 
@@ -16,7 +16,7 @@
 
 #define NUMBERS_SIZE 10
 
-int findFirstOccurrence(int , int *, int);
+int findLastOccurrence(int , int *, int);
 
 int main(void) {
 	setvbuf(stdout, NULL, _IONBF, 0);
@@ -33,18 +33,18 @@ int main(void) {
 	printf("Enter the number you would like to find: ");
 	scanf("%d", &numberToFind);
 
-	findResult = findFirstOccurrence(numberToFind, numbers, NUMBERS_SIZE);
+	findResult = findLastOccurrence(numberToFind, numbers, NUMBERS_SIZE);
 
 	if(findResult== -1){
 		printf("%d was not found in the array", numberToFind);
 	} else {
-		printf("%d first occurrence was at index %d", numberToFind, findResult);
+		printf("%d last occurrence was at index %d", numberToFind, findResult);
 	}
 	return 0;
 }
 
-int findFirstOccurrence(int number,int * array, int arraySize){
-	for(int i = 0 ; i < arraySize; i++){
+int findLastOccurrence(int number,int * array, int arraySize){
+	for(int i = arraySize - 1 ; i >= 0; i--){
 		if(array[i] == number){
 			return i;
 		}
