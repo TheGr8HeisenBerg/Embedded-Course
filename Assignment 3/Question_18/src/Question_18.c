706dc04db372acf87744dc8521990dc0d2aeb49c
TheGr8HeisenBerg <42723307+TheGr8HeisenBerg@users.noreply.github.com>
2021-01-13 11:00:53 +0200

    added solution for quesiton 18

--- Assignment 3/Question_18/src/Question_18.c
+++ Assignment 3/Question_18/src/Question_18.c
@@ -12,9 +12,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int longestConOcc(int * , int , int );
+
 int main(void) {
 	setvbuf(stdout, NULL, _IONBF, 0);
 	setvbuf(stderr, NULL, _IONBF, 0);
 
+	int numbers[] = {1,2,2,3,3,3,3,4,4,4,4,3,3,3};
+	int numbersSize = sizeof(numbers) / sizeof(*numbers);
+	printf("the longest consecutive occurrence of %d in the array is %d",3 , longestConOcc(numbers, numbersSize, 3));
+
 	return 0;
+}
+
+int longestConOcc(int * arr, int arraySize, int number){
+	int longest = 0, j = 0;
+	for(int i = 0;i < arraySize; i++){
+		if(arr[i] == number){
+			j++;
+			if(longest < j){
+				longest = j;
+			}
+		} else {
+			j = 0;
+		}
+	}
+	return longest;
 }
