02e3b3656b2ee86e628b63b244c926a9061b390c
TheGr8HeisenBerg <42723307+TheGr8HeisenBerg@users.noreply.github.com>
2021-01-13 05:37:53 +0200

    added files for question 18

---
+++ Assignment 3/Question_18/src/Question_18.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : Question_18.c
+ Author      : Youssef El-Nemaky
+ Description : Write a C function that return the count of the longest
+ consecutive occurrence of a given number in an array.
+ Example:
+ Array={1,2,2,3,3,3,3,4,4,4,4,3,3,3} and searching for 3 -> result = 4
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+	return 0;
+}
